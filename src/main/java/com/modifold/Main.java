0990f520c1f5ab6774139d0e02e8746ea832250c
bogdanmironov67 <mironovbogdan1354@gmail.com>
2026-04-15 22:26:25 +0300

    Initial commit: Modifold Analytics plugin

---
+++ src/main/java/com/modifold/Main.java
@@ -0,0 +1,24 @@
+package com.modifold;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+        new ModifoldAnalytics("optimized-somehow", "1.0.0");
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+    }
+}
